4e966674785da430d90a25092c814fb4a6059624
圆周率3014 <79385954+yzl3014@users.noreply.github.com>
2026-02-23 12:29:26 +0800

    AIsouler/GKD_subscription
    https://github.com/AIsouler/GKD_subscription

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,12 +4,12 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
+  id: 190456937,
+  name: 'GDK Rules',
   version: 0,
-  author: 'author',
+  author: 'yzl3014',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/yzl3014/gkd/',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
